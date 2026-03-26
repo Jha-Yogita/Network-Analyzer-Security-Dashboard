12dfbd094af775277d392101d682ce64c73e3bb1
Jha-Yogita <yogitajha5@gmail.com>
2026-03-26 19:03:51 +0530

    deployment updates

--- backend/run.py
+++ backend/run.py
@@ -13,7 +13,7 @@ def make_shell_context():
 
 if __name__ == '__main__':
     with app.app_context():
-        db.create_all()  # Create tables
+        db.create_all() 
         print("Database tables created!")
     
     socketio.run(app, debug=True, host='0.0.0.0', port=5000)
